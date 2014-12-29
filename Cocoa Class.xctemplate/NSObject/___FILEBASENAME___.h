fa74d6aa36aa7a328cb89183b6116688e311a5cf
Jay Marcyes <jay@marcyes.com>
2014-12-29 00:43:10 -0700

    initial commit

---
+++ Cocoa Class.xctemplate/NSObject/___FILEBASENAME___.h
@@ -0,0 +1,7 @@
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+
+@import Foundation;
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaSubclass___
+
+@end
---
+++ Cocoa Class.xctemplate/NSObject/___FILEBASENAME___.m
@@ -0,0 +1,7 @@
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
